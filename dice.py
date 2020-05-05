155295847942b423e8e7bfb54d02568191148300
Arslan Nazir <aarsalannazeer@gmail.com>
2020-05-05 15:15:25 +0500

    Initial commit

---
+++ dice.py
@@ -0,0 +1,43 @@
+import random
+
+
+x="y"
+while x=="y":
+    number= random.randint(1,6)
+    if number == 1:
+        print(" ---------- ")
+        print("|          |")
+        print("|    0     |")
+        print("|          |")
+        print(" ---------- ")
+    if number == 2:
+        print(" ---------- ")
+        print("|          |")
+        print("|    0     |")
+        print("|    0     |")
+        print(" ---------- ")
+    if number == 3:
+        print(" ---------- ")
+        print("|    0     |")
+        print("|    0     |")
+        print("|    0     |")
+        print(" ---------- ")
+    if number == 4:
+        print(" ---------- ")
+        print("|    00    |")
+        print("|          |")
+        print("|    00    |")
+        print(" ---------- ")
+    if number == 5:
+        print(" ---------- ")
+        print("|   00     |")
+        print("|    0     |")
+        print("|   00     |")
+        print(" ---------- ")
+    if number == 6:
+        print(" ---------- ")
+        print("|    00    |")
+        print("|    00    |")
+        print("|    00    |")
+        print(" ---------- ")
+    x= input("press y to roll again")
